57436c1c1a709c42849a19d01121f30d4e82eb31
learning-lab-cpp <education.mail.1@protonmail.com>
2020-01-02 12:31:18 +0200

    Arrays Initial Commit

---
+++ src/Arrays/arrays.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int arrName[10] = {1, 2, 3};
+    for(int i = 0; i < 10; i++)
+        std::cout << arrName[i] << " "; // 1 2 3 0 0 0 0 0 0 0
+
+    std::cout << std::endl << arrName[10]; // data found at the next memory address location
+}
