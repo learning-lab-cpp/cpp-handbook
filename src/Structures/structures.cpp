8d77cf567dac2005968b973f0e1c4643edc1f0f0
learning-lab-cpp <education.mail.1@protonmail.com>
2020-01-03 11:13:02 +0200

    Structures Initial Commit

---
+++ src/Structures/structures.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    struct coffeeBean
+    {
+        std::string name;
+        std::string country;
+        int strength;
+    };
+
+    coffeeBean myBean = {"Strata", "Colombia", 10};
+
+    coffeeBean newBean;
+    newBean.name = "Flora";
+    newBean.country = "Mexico";
+    newBean.strength = 9;
+
+    std::cout << "Coffee bean " + myBean.name + " is from " + myBean.country + ". It's strength is " << myBean.strength << "." << std::endl;
+    std::cout << "Coffee bean " + newBean.name + " is from " + newBean.country + ". It's strength is " << newBean.strength << "." << std::endl;
+
+    // Output:
+    // Coffee bean Strata is from Colombia. It's strength is 10.
+    // Coffee bean Flora is from Mexico. It's strength is 9.
+
+}
