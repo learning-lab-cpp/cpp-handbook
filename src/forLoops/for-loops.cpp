de15cc1be25392374c582d53e8a9e5d55ab62abc
learning-lab-cpp <education.mail.1@protonmail.com>
2020-01-11 09:58:27 +0200

    for Loops Initial Commit

---
+++ src/forLoops/for-loops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        // Code to execute.
+        std::cout << i << " ";
+    }
+}
+
+// Output:
+// 1 2 3 4 5 6 7 8 9 10
